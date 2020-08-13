7362aeb1d06c72b325c68ba72d589c4d1ef232ae
Karan <karantandon1991@gmail.com>
2020-08-13 21:13:01 +0530

    printing elements inside of iterator with next

--- cookbook-python-examples/chap4-iterators_and_generators/2.delegating_iteration.py
+++ cookbook-python-examples/chap4-iterators_and_generators/2.delegating_iteration.py
@@ -4,12 +4,21 @@ class ListIterator:
     def __init__(self, value):
         self._value = value
         self.children.append(value)
+        self.i = -1
 
     def __iter__(self):
-        return iter(self.children)
+        return self
+
+    def __next__(self):
+        self.i+=1
+        if self.i >= 5:
+            raise StopIteration
+        return self.children[self.i]
 
     def __repr__(self):
-        return str(self._value)
+        if self.i < 0:
+            return "ListIterator with elements {}".format(str(self.children))
+        return str(self.children[self.i])
 
 a = [1,2,3,4,5]
 
