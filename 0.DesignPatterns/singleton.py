1795b67228930b9f641d391cf16f794aab3aed05
Karan <karantandon1991@gmail.com>
2020-08-13 19:41:11 +0530

    Single with metaclass

--- 0.DesignPatterns/singleton.py
+++ 0.DesignPatterns/singleton.py
@@ -1,37 +1,38 @@
-class Sample(object):
+class Singleton(object):
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super().__new__(cls)
         return cls.instance
 
 
-# class Singleton2(type):
-#     def __new__(cls):
-#         if not hasattr(cls, 'instance'):
-#             cls.instance = super().__new__(cls)
-#         return cls.instance
 
-# class Sample()
-#     def __int__(cls, name, bases, attrs, **kwargs):
-#         print("__init__")
-#         super().__init__(name, bases, attrs)
-#         cls._instance = None
-#
-#     def __call__(cls, *args, **kwargs):
-#         print("__call__")
-#         if cls._instance is None:
-#             cls._instance = super().__call__(*args, **kwargs)
-#         return cls._instance
-#
-#
-#
-# class MyClass(metaclass=Sample):
-#     pass
+class Singleton2(type):
 
-s1 = Sample()
-s2 = Sample()
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+            
+        return cls._instances[cls]
+
+
+
+class MyClass(metaclass=Singleton2):
+    pass
+
+
+
+s1 = Singleton()
+s2 = Singleton()
 
 print(s1 == s2)
 
+S1 = MyClass()
+S2 = MyClass()
+
+print(S1 == S2)
 
 
