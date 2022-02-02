0d2f383773c11a620ca90d9faa656f1924710d6d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-02 17:44:34 +0800

    Delete cookbook-python-examples/chap4-iterators_and_generators directory

--- cookbook-python-examples/chap4-iterators_and_generators/3.itertools_ziplongest.py
+++
@@ -1,9 +0,0 @@
-from itertools import zip_longest
-
-a = [1,2,3,4,5]
-b = ["one", "two"]
-    
-c = zip_longest(a,b)
-
-for i in c:
-    print(i)
--- cookbook-python-examples/chap4-iterators_and_generators/4.itertools_chain_method_for_chaining_seq.py
+++
@@ -1,9 +0,0 @@
-from itertools import chain
-
-a = [1,2,3,4]
-b = ['x', 'y']
-c= chain(a,b)
-
-
-for i in c:
-    print(i)
